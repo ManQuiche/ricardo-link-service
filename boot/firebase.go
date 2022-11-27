43166c70c87dcecdd140f51ea4f7d60e0083d21b
rabotin <mateo.rabotin@dejamobile.com>
2022-11-27 17:02:03 +0100

    caca: caca

--- boot/firebase.go
+++ boot/firebase.go
@@ -14,7 +14,7 @@ var (
 
 func InitFirebaseApp() {
 	var err error
-	opt := option.WithCredentialsFile(firebaseKeyFile)
+	opt := option.WithCredentialsFile("boot/ricardo-9b5d5-firebase-adminsdk-udnxf-2e4b3b051f.json")
 
 	fbLinkService, err = firebasedynamiclinks.NewService(context.Background(), opt)
 	if err != nil {
